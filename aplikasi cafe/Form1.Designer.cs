be00da67faa3e61594dd5990a7a8baff695d0401
BMawar <bungamawar040905@gmail.com>
2024-05-22 09:26:57 +0700

    Add project files.

---
+++ aplikasi cafe/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace aplikasi_cafe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BYR = new System.Windows.Forms.Label();
+            this.TOTAL = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.milk = new System.Windows.Forms.TextBox();
+            this.TOTALBYR = new System.Windows.Forms.TextBox();
+            this.UANGYGDBYR = new System.Windows.Forms.TextBox();
+            this.sugar = new System.Windows.Forms.TextBox();
+            this.matcha = new System.Windows.Forms.TextBox();
+            this.velvet = new System.Windows.Forms.TextBox();
+            this.capucino = new System.Windows.Forms.TextBox();
+            this.PMBYRN = new System.Windows.Forms.TextBox();
+            this.PAJAK = new System.Windows.Forms.TextBox();
+            this.KMBLIAN = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DHIRA CAFE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LIST MENU";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "COFFE MILK";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CAPPUCINO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "REDVELVET";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(23, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "MATCHALATTE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(23, 372);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "BROWN SUGAR";
+            // 
+            // BYR
+            // 
+            this.BYR.AutoSize = true;
+            this.BYR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BYR.Location = new System.Drawing.Point(327, 180);
+            this.BYR.Name = "BYR";
+            this.BYR.Size = new System.Drawing.Size(150, 15);
+            this.BYR.TabIndex = 7;
+            this.BYR.Text = "UANG YANG DI BAYAR";
+            // 
+            // TOTAL
+            // 
+            this.TOTAL.AutoSize = true;
+            this.TOTAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TOTAL.Location = new System.Drawing.Point(327, 231);
+            this.TOTAL.Name = "TOTAL";
+            this.TOTAL.Size = new System.Drawing.Size(96, 15);
+            this.TOTAL.TabIndex = 8;
+            this.TOTAL.Text = "TOTAL BAYAR";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(327, 282);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 15);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "PAJAK 5%";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(327, 328);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(98, 15);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "PEMBAYARAN";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(327, 417);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(84, 15);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "KEMBALIAN";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(330, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(311, 26);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "PROSES";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // milk
+            // 
+            this.milk.Location = new System.Drawing.Point(160, 171);
+            this.milk.Name = "milk";
+            this.milk.Size = new System.Drawing.Size(100, 20);
+            this.milk.TabIndex = 13;
+            // 
+            // TOTALBYR
+            // 
+            this.TOTALBYR.Location = new System.Drawing.Point(541, 226);
+            this.TOTALBYR.Name = "TOTALBYR";
+            this.TOTALBYR.Size = new System.Drawing.Size(100, 20);
+            this.TOTALBYR.TabIndex = 14;
+            // 
+            // UANGYGDBYR
+            // 
+            this.UANGYGDBYR.Location = new System.Drawing.Point(541, 175);
+            this.UANGYGDBYR.Name = "UANGYGDBYR";
+            this.UANGYGDBYR.Size = new System.Drawing.Size(100, 20);
+            this.UANGYGDBYR.TabIndex = 15;
+            // 
+            // sugar
+            // 
+            this.sugar.Location = new System.Drawing.Point(160, 367);
+            this.sugar.Name = "sugar";
+            this.sugar.Size = new System.Drawing.Size(100, 20);
+            this.sugar.TabIndex = 16;
+            // 
+            // matcha
+            // 
+            this.matcha.Location = new System.Drawing.Point(160, 314);
+            this.matcha.Name = "matcha";
+            this.matcha.Size = new System.Drawing.Size(100, 20);
+            this.matcha.TabIndex = 17;
+            // 
+            // velvet
+            // 
+            this.velvet.Location = new System.Drawing.Point(160, 264);
+            this.velvet.Name = "velvet";
+            this.velvet.Size = new System.Drawing.Size(100, 20);
+            this.velvet.TabIndex = 18;
+            // 
+            // capucino
+            // 
+            this.capucino.Location = new System.Drawing.Point(160, 215);
+            this.capucino.Name = "capucino";
+            this.capucino.Size = new System.Drawing.Size(100, 20);
+            this.capucino.TabIndex = 19;
+            // 
+            // PMBYRN
+            // 
+            this.PMBYRN.Location = new System.Drawing.Point(541, 323);
+            this.PMBYRN.Name = "PMBYRN";
+            this.PMBYRN.Size = new System.Drawing.Size(100, 20);
+            this.PMBYRN.TabIndex = 20;
+            // 
+            // PAJAK
+            // 
+            this.PAJAK.Location = new System.Drawing.Point(541, 277);
+            this.PAJAK.Name = "PAJAK";
+            this.PAJAK.Size = new System.Drawing.Size(100, 20);
+            this.PAJAK.TabIndex = 21;
+            // 
+            // KMBLIAN
+            // 
+            this.KMBLIAN.Location = new System.Drawing.Point(541, 412);
+            this.KMBLIAN.Name = "KMBLIAN";
+            this.KMBLIAN.Size = new System.Drawing.Size(100, 20);
+            this.KMBLIAN.TabIndex = 22;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::aplikasi_cafe.Properties.Resources.coffe;
+            this.pictureBox1.Location = new System.Drawing.Point(330, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.KMBLIAN);
+            this.Controls.Add(this.PAJAK);
+            this.Controls.Add(this.PMBYRN);
+            this.Controls.Add(this.capucino);
+            this.Controls.Add(this.velvet);
+            this.Controls.Add(this.matcha);
+            this.Controls.Add(this.sugar);
+            this.Controls.Add(this.UANGYGDBYR);
+            this.Controls.Add(this.TOTALBYR);
+            this.Controls.Add(this.milk);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.TOTAL);
+            this.Controls.Add(this.BYR);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label BYR;
+        private System.Windows.Forms.Label TOTAL;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox milk;
+        private System.Windows.Forms.TextBox TOTALBYR;
+        private System.Windows.Forms.TextBox UANGYGDBYR;
+        private System.Windows.Forms.TextBox sugar;
+        private System.Windows.Forms.TextBox matcha;
+        private System.Windows.Forms.TextBox velvet;
+        private System.Windows.Forms.TextBox capucino;
+        private System.Windows.Forms.TextBox PMBYRN;
+        private System.Windows.Forms.TextBox PAJAK;
+        private System.Windows.Forms.TextBox KMBLIAN;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ aplikasi cafe/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aplikasi_cafe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double menucoffeMilk, menucappucino, menuredVelved, menumatchaLatte, menubrownSugar, menutotalBayar, menuuang, menupajak, menukembalian;
+            double hargacoffeMilk, hargacappucino, hargaredVelvet, hargamatchaLatte, hargabrownSugar, menupembayaran;
+
+            menucoffeMilk = double.Parse(milk.Text);
+            menucappucino = double.Parse(capucino.Text);
+            menuredVelved = double.Parse(velvet.Text);
+            menumatchaLatte = double.Parse(matcha.Text);
+            menubrownSugar = double.Parse(sugar.Text);
+            menuuang = double.Parse(UANGYGDBYR.Text);
+
+            hargacoffeMilk = menucoffeMilk * 10000;
+            hargacappucino = menucappucino * 11000;
+            hargaredVelvet = menuredVelved * 12000;
+            hargamatchaLatte = menumatchaLatte * 13000;
+            hargabrownSugar = menubrownSugar * 14000;
+
+            menutotalBayar = hargacoffeMilk + hargacappucino + hargaredVelvet + hargamatchaLatte + hargabrownSugar;
+            menupajak = menutotalBayar * 0.05;
+
+            menupembayaran = menutotalBayar + menupajak;
+            menukembalian = menuuang - menupembayaran;
+
+            TOTALBYR.Text = menutotalBayar.ToString();
+            PAJAK.Text = menupajak.ToString();
+            PMBYRN.Text = menupembayaran.ToString();
+            KMBLIAN.Text = menukembalian.ToString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        }
+    }
+}
